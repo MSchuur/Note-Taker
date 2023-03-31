a0cf8017e6b463e79ad6753ee64f3c296fb36b6f
MSchuur <mschuur31@gmail.com>
2023-03-31 07:00:52 -0300

    Formatted the files to present better and unified. Added comments.

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,24 +1,20 @@
+// Import Express and required modules
 const htmlRoute = require('express').Router();
 const path = require('path');
 
-  
+// HTML Routes 
 htmlRoute.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
   
- // HTML Routes
- htmlRoute.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-  });
+htmlRoute.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
 
+// Catch all route
+htmlRoute.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
- htmlRoute.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
- 
-//   htmlRoute.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/index.html'))
-// );
-  
+// Export this route 
 module.exports = htmlRoute;
